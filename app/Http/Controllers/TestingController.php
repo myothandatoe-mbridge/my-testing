d6dd3a2bfcc7735e697acff21426c514a45237ab
myothandatoe-mbridge <myo.thanda.toe@frobom.com>
2023-04-27 14:49:15 +0630

    initial commit

---
+++ app/Http/Controllers/TestingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestingController extends Controller
+{
+    /**
+    * Display a listing of the resource.
+    *
+    * @param  string  $post_type
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+    	'foo';
+        return view('welcome');
+    }
+}
