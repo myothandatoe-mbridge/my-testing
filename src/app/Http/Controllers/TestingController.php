4ecac7ff5104f4c45dbc20a7e21e26edc4fa0993
myothandatoe-mbridge <myo.thanda.toe@frobom.com>
2023-04-28 09:53:35 +0630

    #4 testing error

--- src/app/Http/Controllers/TestingController.php
+++ src/app/Http/Controllers/TestingController.php
@@ -6,8 +6,15 @@ use Illuminate\Http\Request;
 
 class TestingController extends Controller
 {
+	/**
+	* Display a listing of the resource.
+	*
+	* @param  string  $post_type
+	* @return \Illuminate\Http\Response
+	*/
     public function index()
     {
+    	'latest';
         return view('welcome');
     }
 }
